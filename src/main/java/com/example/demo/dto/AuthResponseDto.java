960f1173c649759a566de4483b02c67a55a18dde
bala-amypo <bala@amypo.in>
2025-12-18 08:44:20 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponseDto.java
+++ src/main/java/com/example/demo/dto/AuthResponseDto.java
@@ -4,7 +4,8 @@ public class AuthResponseDto {
 
     private String token;
 
-    public AuthResponseDto() { }
+    public AuthResponseDto() {
+    }
 
     public AuthResponseDto(String token) {
         this.token = token;
@@ -13,7 +14,7 @@ public class AuthResponseDto {
     public String getToken() {
         return token;
     }
-
+    
     public void setToken(String token) {
         this.token = token;
     }
--- src/main/java/com/example/demo/dto/RegisterRequestDto.java
+++ src/main/java/com/example/demo/dto/RegisterRequestDto.java
@@ -6,12 +6,13 @@ public class RegisterRequestDto {
     private String password;
     private String role;
 
-    public RegisterRequestDto() { }
+    public RegisterRequestDto() {
+    }
 
     public String getEmail() {
         return email;
     }
-
+    
     public void setEmail(String email) {
         this.email = email;
     }
@@ -19,7 +20,7 @@ public class RegisterRequestDto {
     public String getPassword() {
         return password;
     }
-
+    
     public void setPassword(String password) {
         this.password = password;
     }
@@ -27,7 +28,7 @@ public class RegisterRequestDto {
     public String getRole() {
         return role;
     }
-
+    
     public void setRole(String role) {
         this.role = role;
     }
