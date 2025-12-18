8c639252ec3443bcc2edb0b4be8a937054a41c30
bala-amypo <bala@amypo.in>
2025-12-18 05:05:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/KeyExemptionRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.KeyExemption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KeyExemptionRepository extends JpaRepository<KeyExemption, Long> {
+
+    KeyExemption findByApiKey_Id(Long id);
+}
