46fcfa3a1c70de6b6dc94ac218d28a5a06a25e83
bala-amypo <bala@amypo.in>
2025-12-19 05:50:57 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.*;
-import io.swagger.v3.oas.models.security.*;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,12 +17,11 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("API Rate Limiter & Quota Manager API")
                         .version("1.0")
-                        .description("API key management, quota plans, usage logs, rate limiting"))
+                        .description("API key management, quota plans, usage logging, rate limit enforcement, and exemptions"))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth",
                                 new SecurityScheme()
-                                        .name("bearerAuth")
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
