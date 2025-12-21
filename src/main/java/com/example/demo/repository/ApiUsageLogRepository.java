2c2bf677fc9c3c97f5b2eba491f7d551a8677ef3
bala-amypo <bala@amypo.in>
2025-12-21 18:22:38 +0000

    User commit

--- src/main/java/com/example/demo/repository/ApiUsageLogRepository.java
+++ src/main/java/com/example/demo/repository/ApiUsageLogRepository.java
@@ -2,8 +2,15 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.ApiUsageLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface ApiUsageLogRepository extends JpaRepository<ApiUsageLog, Long> {
-    List<ApiUsageLog> findByApiKey_Id(Long id);
+
+    List<ApiUsageLog> findByApiKeyId(Long id);
+
+    List<ApiUsageLog> findByApiKeyIdAndTimestampBetween(Long id, Timestamp start, Timestamp end);
+
+    long countByApiKeyIdAndTimestampBetween(Long id, Timestamp start, Timestamp end);
 }
