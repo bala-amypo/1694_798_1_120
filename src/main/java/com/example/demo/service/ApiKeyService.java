3e3e5d9f41a2a48b21fdecd3ecd31dc738e0cc63
bala-amypo <bala@amypo.in>
2025-12-21 17:57:45 +0000

    User commit

--- src/main/java/com/example/demo/service/ApiKeyService.java
+++ src/main/java/com/example/demo/service/ApiKeyService.java
@@ -2,7 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.dto.ApiKeyDto;
 import java.util.List;
-
+@Service
 public interface ApiKeyService {
     ApiKeyDto createApiKey(ApiKeyDto dto);
     ApiKeyDto updateApiKey(Long id, ApiKeyDto dto);
