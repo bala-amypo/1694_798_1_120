02cb4a48f619704ec90ddbffa3dc53ff07656086
bala-amypo <bala@amypo.in>
2025-12-22 10:14:42 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,25 +2,35 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
         return new OpenAPI()
                 .info(new Info()
                         .title("API Rate Limiter & Quota Manager")
                         .version("1.0.0")
                         .description("Manages API keys, quota plans, usage logs, and rate limits")
                 )
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
                 .servers(List.of(
-                        new Server().url("https://9180.408procr.amypo.ai/")
-                ));
+                    new Server().url("https://9180.408procr.amypo.ai/")
+                ))
     }
 }
