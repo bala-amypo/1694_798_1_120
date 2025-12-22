2025c3b8c54be982b5bcc44078a6f0cc0278ae11
bala-amypo <bala@amypo.in>
2025-12-22 09:23:43 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,9 +18,9 @@ public class SwaggerConfig {
                         .title("API Rate Limiter & Quota Manager")
                         .version("1.0.0")
                         .description("Manages API keys, quota plans, usage logs, and rate limits")
-                );
-                .server(List.of(
-                    new Server().url("https://9180.408procr.amypo.ai/")
+                )
+                .servers(List.of(
+                        new Server().url("https://9180.408procr.amypo.ai/")
                 ));
     }
 }
