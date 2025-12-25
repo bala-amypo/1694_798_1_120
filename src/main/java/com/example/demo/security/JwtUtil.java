3f9b4c3a650ff7ef58930a5a2a728160850297ea
bala-amypo <bala@amypo.in>
2025-12-25 05:34:53 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -26,3 +26,33 @@ public class JwtUtil {
         return 3600000;
     }
 }
+package com.example.demo.security;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+
+    public String generateToken(Map<String, Object> claims, String subject) {
+        return "JWT_TOKEN";
+    }
+
+    public Claims getClaims(String token) {
+        return null;
+    }
+
+    public String getUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, String username) {
+        return true;
+    }
+
+    public long getExpirationMillis() {
+        return 86400000L;
+    }
+}
